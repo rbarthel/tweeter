5b223903b6848dc032f486e6a0db1cfe1394af34
Russell Barthel <russell.barthel@gmail.com>
2017-09-19 20:45:14 +0000

    update css styling

--- public/scripts/tweet-on-hover.js
+++ public/scripts/tweet-on-hover.js
@@ -1,9 +1,12 @@
 $( document ).ready(function() {
   $('.tweet-buttons').css('visibility','hidden');
+
   $('.all-tweets').on('mouseenter', function() {
     $(this).find('.tweet-buttons').css('visibility','visible');
+    $(this).find('header').css('opacity','1');
   });
   $('.all-tweets').on('mouseleave', function() {
     $(this).find('.tweet-buttons').css('visibility','hidden');
+    $(this).find('header').css('opacity','0.75');
   });
 });
